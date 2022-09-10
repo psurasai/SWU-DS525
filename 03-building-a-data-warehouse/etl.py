4ea787b8721a497dd1d45209451348ff0bfeb399
psurasai <peerawit.ty@gmail.com>
2022-09-10 10:30:48 +0000

    Conect to Redshift

--- 03-building-a-data-warehouse/etl.py
+++ 03-building-a-data-warehouse/etl.py
@@ -1,106 +1,93 @@
-from typing import NewType
-
 import psycopg2
 
 
-PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
-PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
-
-table_drop_events = "DROP TABLE IF EXISTS events"
-table_drop_actors = "DROP TABLE IF EXISTS actors"
-table_drop_repos = "DROP TABLE IF EXISTS repos"
-table_drop_orgs = "DROP TABLE IF EXISTS orgs"
-
-
-table_create_actors = """
-    CREATE TABLE IF NOT EXISTS actors (
-        id int,
-        login text,
-        PRIMARY KEY(id)
-    )
-"""
-
-table_create_repos = """
-    CREATE TABLE IF NOT EXISTS repos (
-        id int,
-        name text,
-        url VARCHAR,
-        PRIMARY KEY(id)
-    )
-"""
-table_create_orgs = """
-    CREATE TABLE IF NOT EXISTS orgs (
-        id int,
-        login text,
-        gravatar_id int,
-        url VARCHAR,
-        vatar_url VARCHAR,
-        PRIMARY KEY(id)
-    )
-"""
-
-table_create_events = """
-    CREATE TABLE IF NOT EXISTS events (
+drop_table_queries = [
+    "DROP TABLE IF EXISTS events",
+]
+create_table_queries = [
+    """
+    CREATE TABLE IF NOT EXISTS staging_events (
         id text,
         type text,
-        actor_id int,
-        repos_id int,
-        orgs_id int,
-        PRIMARY KEY(id),
-        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id),
-        CONSTRAINT fk_repos FOREIGN KEY(repos_id) REFERENCES repos(id),
-        CONSTRAINT fk_orgs FOREIGN KEY(orgs_id) REFERENCES orgs(id)
+        actor text,
+        repo text,
+        created_at text
     )
-"""
-
-create_table_queries = [
-    table_create_actors,
-    table_create_orgs,
-    table_create_repos,
-    table_create_events
-]
-drop_table_queries = [
-    table_drop_events,
-    table_drop_actors,
-    table_drop_repos,
-    table_drop_orgs
-]
-
-
-def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+    """,
     """
-    Drops each table using the queries in `drop_table_queries` list.
+    CREATE TABLE IF NOT EXISTS events (
+        id int
+    )
+    """,
+]
+copy_table_queries = [
     """
+    COPY staging_events FROM 's3://zkan-swu-labs/github_events_01.json'
+    CREDENTIALS 'aws_iam_role=arn:aws:iam::377290081649:role/LabRole'
+    JSON 's3://zkan-swu-labs/events_json_path.json'
+    REGION 'us-east-1'
+    """,
+]
+insert_table_queries = [
+    """
+    INSERT INTO
+      events (
+        id
+      )
+    SELECT
+      DISTINCT id,
+    FROM
+      staging_events
+    WHERE
+      id NOT IN (SELECT DISTINCT id FROM events)
+    """,
+]
+
+
+def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
-    """
-    Creates each table using the queries in `create_table_queries` list.
-    """
+def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
 def main():
-    """
-    - Drops (if exists) and Creates the sparkify database.
-    - Establishes connection with the sparkify database and gets
-    cursor to it.
-    - Drops all the tables.
-    - Creates all tables needed.
-    - Finally, closes the connection.
-    """
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
-    )
+    host = "redshift-cluster-1.cak93pn3g0gg.us-east-1.redshift.amazonaws.com"
+    dbname = "dev"
+    user = "awsuser"
+    password = "MB93ei854"
+    port = "5439"
+    conn_str = f"host={host} dbname={dbname} user={user} password={password} port={port}"
+    conn = psycopg2.connect(conn_str)
     cur = conn.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    # drop_tables(cur, conn)
+    # create_tables(cur, conn)
+    # load_tables(cur, conn)
+    # insert_tables(cur, conn)
+
+    query = "SELECT * FROM category"
+    cur.execute(query)
+    records = cur.fetchall()
+    for row in records:
+        print(row)
 
     conn.close()
 
