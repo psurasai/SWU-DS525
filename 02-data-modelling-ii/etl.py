f54a104eded174ca67cea3a05b3836e0a00ce1c9
psurasai <peerawit.ty@gmail.com>
2022-10-02 10:15:16 +0000

    Updated ETL

--- 02-data-modelling-ii/etl.py
+++ 02-data-modelling-ii/etl.py
@@ -5,27 +5,45 @@ from typing import List
 
 from cassandra.cluster import Cluster
 
+#drop the exist tables
+table_drop_events = "DROP TABLE events"
+table_drop_actors = "DROP TABLE actors"
 
-table_drop = "DROP TABLE events"
-
-table_create = """
+#create tables
+table_create_events = """
     CREATE TABLE IF NOT EXISTS events
     (
-        id text,
+        id bigint,
         type text,
+        actor_id bigint,
+        actor text,
         public boolean,
+        created_at timestamp,
         PRIMARY KEY (
-            id,
-            type
+            (actor_id),
+            created_at
+        )
+    )
+"""
+table_create_actors = """
+    CREATE TABLE IF NOT EXISTS actors
+    (
+        actor_id bigint,
+        actor text,
+        number_events int,
+        PRIMARY KEY (
+            actor_id
         )
     )
 """
 
 create_table_queries = [
-    table_create,
+    table_create_events,
+    table_create_actors
 ]
 drop_table_queries = [
-    table_drop,
+    table_drop_events,
+    table_drop_actors
 ]
 
 def drop_tables(session):
@@ -62,6 +80,9 @@ def get_files(filepath: str) -> List[str]:
 
 
 def process(session, filepath):
+    """
+    Description: This function is used for insert data into events table
+    """
     # Get list of files from filepath
     all_files = get_files(filepath)
 
@@ -70,19 +91,51 @@ def process(session, filepath):
             data = json.loads(f.read())
             for each in data:
                 # Print some sample data
-                print(each["id"], each["type"], each["actor"]["login"])
+                #print(each["id"], each["type"],each["public"],each["created_at"])
 
                 # Insert data into tables here
+                query_events = f"""
+                    INSERT INTO events (
+                        id,
+                        type,
+                        actor_id,
+                        actor,
+                        public,
+                        created_at
+                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}', '{each["actor"]["login"]}',
+                    {each["public"]},'{each["created_at"]}');
+                    """
+                session.execute(query_events)
 
 
 def insert_sample_data(session):
-    query = f"""
-    INSERT INTO events (id, type, public) VALUES ('23487929637', 'IssueCommentEvent', true)
     """
-    session.execute(query)
+    Description: This function is used for query data from events table and count the events of each actors.
+    Then, the query data are inserted into actors table.
+    """
+    #Query data from event table and count number of the events of each actor
+    count_actors = """
+    SELECT actor_id, actor, count(actor_id) as count_no from events GROUP BY actor_id ALLOW FILTERING
+    """
 
+    try:
+        rows = session.execute(count_actors)
+        for row in rows:
+        # Insert data into actor tables here
+            query_actors = f"""
+                INSERT INTO actors (
+                actor_id,
+                actor,
+                number_events
+                ) VALUES ('{row[0]}', '{row[1]}', {row[2]});
+                """
+            session.execute(query_actors)
+        #print(row)
+    except Exception as e:
+        print(e)
 
 def main():
+    #Connect to cluster
     cluster = Cluster(['127.0.0.1'])
     session = cluster.connect()
 
@@ -103,17 +156,21 @@ def main():
     except Exception as e:
         print(e)
 
+    #drop and create table
     drop_tables(session)
     create_tables(session)
 
-    # process(session, filepath="../data")
+    #insert data to events and actors tables
+    process(session, filepath="../data")
     insert_sample_data(session)
 
     # Select data in Cassandra and print them to stdout
+    #Try to query data from event
     query = """
-    SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
+    SELECT * from events WHERE type = 'PushEvent' ALLOW FILTERING
     """
     try:
+        print("query PushEvent")
         rows = session.execute(query)
     except Exception as e:
         print(e)
@@ -121,6 +178,20 @@ def main():
     for row in rows:
         print(row)
 
+    # Select data in Cassandra and print them
+    #Try to query data from actors
+    query = """
+    SELECT actor_id, actor, number_events from actors 
+    WHERE number_events > 1 ALLOW FILTERING
+    """
+    try:
+        print("query number of events by each actor if number of event more than 1")
+        rows = session.execute(query)
+    except Exception as e:
+        print(e)
+
+    for row in rows:
+        print(row)
 
 if __name__ == "__main__":
     main()
